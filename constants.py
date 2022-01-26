60e3d3cba8c3b7ca151aed3e129359ba0697712b
Ian12467 <64345032+Ian12467@users.noreply.github.com>
2022-01-26 16:01:17 +0300

    constants

---
+++ constants.py
@@ -0,0 +1,18 @@
+#constant
+# string
+name =input("Enter your name: ")
+print (name)
+
+#numeric
+age =int(input("Enter your age: "))
+print(type(age))
+print(age)
+height=float(input("Enter your height: "))
+print(type(height))
+print(height)
+weight=float(input("Enter your weight: "))
+print(type(weight))
+print(weight)
+#discount
+discount =float(input("Enter a discount: "))
+print(discount)
