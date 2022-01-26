db1f795b368dc598ccc5fc2f14295843eed7677d
Ian12467 <64345032+Ian12467@users.noreply.github.com>
2022-01-26 15:56:05 +0300

    Add files via upload
    User enter name

---
+++ userinput_sum.py
@@ -0,0 +1,6 @@
+#find the sum of 2 numbers
+a= int(input("Enter value a:"))
+b= int(input("Enter value b:"))
+# int(a)/ another way to change string to integer
+sum=a+b
+print("The sum of a and b is",sum)
