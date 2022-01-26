48b8b817bf6aa71ae25965df7d4ead5e1d90843b
Ian12467 <64345032+Ian12467@users.noreply.github.com>
2022-01-26 16:06:26 +0300

    login system

---
+++ simple login.py
@@ -0,0 +1,5 @@
+#Simple login sytem
+name=input ("Enter name: ") #to give a prompt to in put name
+username=input ("Username: ") #to give a prompt to in put username
+password=input ("Enter password: ") #to give a prompt to in password
+print("WELCOME TO MY CHANNEL  ",name) #runs after the input is given
