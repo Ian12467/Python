4b9496cc334a6b954bf105f81547a15aa7ebcd21
Ian12467 <64345032+Ian12467@users.noreply.github.com>
2022-01-26 16:04:56 +0300

    simple constants I got

---
+++ simple constants.py
@@ -0,0 +1,12 @@
+#constant
+# string
+name =input("Enter your name: ")
+print (name)
+
+#numeric
+age =int(input("Enter your age: "))
+print(type(age))
+print(age)
+#discount
+discount =float(input("Enter a discount: "))
+print(discount)
